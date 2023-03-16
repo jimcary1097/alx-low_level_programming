99406725132287e796c7eae682997fcbeec84593
Ahmed mustafa <jimcary1097@gmail.com>
2023-03-16 11:59:17 -0700

    s

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,12 +1,6 @@
 #include <stdio.h>
-/**
- * main - Entry point
- *
- * Description: Uses the function "puts" to write to stdout
- * Return: Always 0 (Success)
- */
 int main(void)
-{  
+{
 	puts("\"Programming is like building a multilingual puzzle");
 	return (0);
 }
--- 0x00-hello_world/6-size.c
+++ 0x00-hello_world/6-size.c
@@ -1,7 +1,7 @@
 #include <stdio.h>
-int main(void) 
+int main(void)
 {
-	char a; 
+	char a;
 	int b;
 	long int c;
 	long long int d;
@@ -10,6 +10,6 @@ int main(void)
 	printf("Size of an int: %lu byte(s)\n", (unsigned long)sizeof(b));
 	printf("Size of a long int: %lu byte(s)\n", (unsigned long)sizeof(c));
 	printf("Size of a long long int: %lu byte(s)\n", (unsigned long)sizeof(d));
-       	printf("Size of a float: %lu byte(s)\n", (unsigned long)sizeof(e));
-	return (0); 
+	printf("Size of a float: %lu byte(s)\n", (unsigned long)sizeof(e));
+	return (0);
 }

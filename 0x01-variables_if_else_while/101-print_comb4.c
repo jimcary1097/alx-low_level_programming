bacbbbb1f187b12a60b8b56cd98fd3e52f226a91
jimcary1097 <49244712+jimcary1097@users.noreply.github.com>
2023-03-18 16:21:53 +0300

    Update 101-print_comb4.c

--- 0x01-variables_if_else_while/101-print_comb4.c
+++ 0x01-variables_if_else_while/101-print_comb4.c
@@ -1,1 +1,40 @@
-#include <stdio.h>  /**  * main - Entry point  *  * Return: Always 0 (Success)  */ int main(void) {  int firstnum, secondnum, thirdnum;   firstnum = 48;  while (firstnum < 58)  {   secondnum = 49;   while (secondnum < 58)   {    thirdnum = 50;    while (thirdnum < 58)    {     if (thirdnum > secondnum && secondnum > firstnum)     {      putchar(firstnum);      putchar(secondnum);      putchar(thirdnum);      if (firstnum != 55 || secondnum != 56)      {       putchar(',');       putchar(' ');      }     }     thirdnum++;    }    secondnum++;   }   firstnum++;  }  putchar('\n');  return (0); }
+#include <stdio.h>
+
+/**
+ * main - Entry point
+ *
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+	int firstnum, secondnum, thirdnum;
+
+	firstnum = 48;
+	while (firstnum < 58)
+	{
+		secondnum = 49;
+		while (secondnum < 58)
+		{
+			thirdnum = 50;
+			while (thirdnum < 58)
+			{
+				if (thirdnum > secondnum && secondnum > firstnum)
+				{
+					putchar(firstnum);
+					putchar(secondnum);
+					putchar(thirdnum);
+					if (firstnum != 55 || secondnum != 56)
+					{
+						putchar(',');
+						putchar(' ');
+					}
+				}
+				thirdnum++;
+			}
+			secondnum++;
+		}
+		firstnum++;
+	}
+	putchar('\n');
+	return (0);
+}

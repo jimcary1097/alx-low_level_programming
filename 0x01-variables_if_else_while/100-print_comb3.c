c0ade18a7a4fb9070d989cc9d187a275e4f798e2
jimcary1097 <49244712+jimcary1097@users.noreply.github.com>
2023-03-18 16:22:22 +0300

    Update 100-print_comb3.c

--- 0x01-variables_if_else_while/100-print_comb3.c
+++ 0x01-variables_if_else_while/100-print_comb3.c
@@ -1,1 +1,34 @@
-#include <stdio.h>  /**  * main - Entry point  *  * Return: Always 0 (Success)  */ int main(void) { int first_num, second_num;   first_num = 48;  while (first_num <= 57)  {   second_num = 48;   while (second_num <= 57)   {    if (second_num > first_num)    {     putchar(first_num);     putchar(second_num);     if (first_num != 56 || second_num != 57)     {      putchar(',');      putchar(' ');     }    }    second_num++;   }   first_num++;  }  putchar('\n');  return (0); }
+#include <stdio.h>
+
+/**
+ * main - Entry point
+ *
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+int first_num, second_num;
+
+	first_num = 48;
+	while (first_num <= 57)
+	{
+		second_num = 48;
+		while (second_num <= 57)
+		{
+			if (second_num > first_num)
+			{
+				putchar(first_num);
+				putchar(second_num);
+				if (first_num != 56 || second_num != 57)
+				{
+					putchar(',');
+					putchar(' ');
+				}
+			}
+			second_num++;
+		}
+		first_num++;
+	}
+	putchar('\n');
+	return (0);
+}

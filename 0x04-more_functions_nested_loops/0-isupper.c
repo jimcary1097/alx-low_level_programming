473a6e1da1a3cee34bc986077d0e24f14f8c4258
jimcary1097 <49244712+jimcary1097@users.noreply.github.com>
2023-03-24 20:59:44 +0300

    Update 0-isupper.c

--- 0x04-more_functions_nested_loops/0-isupper.c
+++ 0x04-more_functions_nested_loops/0-isupper.c
@@ -8,17 +8,8 @@
  */
 int _isupper(int c)
 {
-	char uppercase = 'A';
-	int isupper = 0;
-
-	for (; uppercase <= 'Z'; uppercase++)
-	{
-		if (c == uppercase)
-		{
-			isupper = 1;
-			break;
-		}
-	}
-
-	return (isupper);
+	if (c >= 65 && c <= 90)
+		return (1);
+	else
+		return (0);
 }

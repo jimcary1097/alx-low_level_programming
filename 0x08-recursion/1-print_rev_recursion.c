a070e688391800c83806e5c841bc05a730bc2b8f
jimcary1097 <49244712+jimcary1097@users.noreply.github.com>
2023-04-07 17:06:54 +0300

    Update 1-print_rev_recursion.c

---
+++ 0x08-recursion/1-print_rev_recursion.c
@@ -0,0 +1,14 @@
+/**
+ * _print_rev_recursion - prints a string in reverse
+ * @s: An input string to print
+ *
+ * Return: void
+ */
+void _print_rev_recursion(char *s)
+{
+	if (*s)
+	{
+		_print_rev_recursion(s + 1);
+		_putchar(*s);
+	}
+}
